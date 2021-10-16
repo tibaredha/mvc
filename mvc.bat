b483c6d4ceced0819e296619d91c705b8c8fe305
tibaredha <tibaredha@yahoo.fr>
2021-10-16 08:41:07 +0100

    maj add mvc.bat

---
+++ mvc.bat
@@ -0,0 +1,107 @@
+::tiba batch
+:: tiba batch
+::@echo off
+title TIBA BATCH
+mode con cols=80 lines=10
+color 1F
+
+::Cette ligne permet de cacher toutes les lignes de commande qui sont effectuées lors de l'exécution du programme, ainsi que cette même ligne
+::echo Bonjour Monde, ceci est mon premier programme en Batch :)
+::1 les variables du script 
+
+
+
+:: a prend pour valeur PifyZ
+::set a=PifyZ
+
+:: b prend pour valeur 2+5, soit 7
+::set /a b=2+5
+
+:: c prend pour valeur un nombre aléatoire
+::set /a c=%RANDOM%
+
+:: d prend pour valeur 10
+::set /a d=%b%+3
+
+:: e prend pour valeur la saisie de l'utilisateur
+::set /p e=Quel est votre pseudo ?
+
+:: Affiche le contenu de la variable d (ici elle vaut 10)
+::echo %d%
+
+:: Affiche le "f" contenu dans la variable a
+::echo %a:~2,1%
+
+:: Affiche l'heure de l'ordinateur
+::echo %TIME%
+::echo %c%
+
+
+::2 les﻿‌ variables d'environnement
+
+
+
+::rmdir hello_les_zeros
+::mkdir hello_les_zeros
+::cd hello_les_zeros
+::echo tibaredha >  tiba.txt
+::echo amranemimi >> tiba.txt
+::ren tiba.txt amrane.txt
+::cd ..
+
+::cd hello_les_zeros
+::type amrane.txt
+::cd ..
+
+::cd hello_les_zeros
+::start hello_les_zeros
+::start amrane.txt
+::start http://localhost/framework/
+::del D/framework/libs/sessions/*.*
+
+D:
+
+cd /wamp/www/mvc/
+
+::del *.* /q
+
+::cd ..
+
+::cd ..
+
+git status
+
+git add .
+
+git commit -a -m "maj"
+
+git push
+
+D:
+
+cd /wamp/bin/mysql/mysql5.1.36/data/
+
+git status
+
+git add .
+
+git commit -a -m "maj"
+
+git push
+
+start http://localhost/mvc/
+
+::pause > nul
+
+exit
+
+
+
+
+
+
+
+
+
+
+
